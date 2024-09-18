5fbf11358791844523d563ae318e797552a01fdf
Yash Gupta <apple@apples-MacBook-Pro.local>
2024-09-18 15:17:21 +0530

    day2 commiting

---
+++ Day 2/objectarray.js
@@ -0,0 +1,38 @@
+const arr = [1,2,3,4,5,6,7,8,9,10];
+
+const even = arr.filter((evennum)=>evennum%2==0);
+console.log(even);
+
+// const details= [
+// {
+// 	name : "yash",
+// 	address : "udaipur",
+// 	phone: 23412
+// },
+// {
+// 	name:"sahil",
+// 	address: "delhi",
+// 	phone: 66543
+// },
+// {
+// 	name:"rahul",
+// 	address: "alwar",
+// 	phone: 45678
+// }
+// ]
+
+
+// // const values = detail.map(item=> {
+// // 	return `${item.name} : ${item.address}`;
+// // })
+
+// // console.log(values);
+
+
+// // detail.forEach((item)=>{
+// // 	process.stdout.write(`name: ${item.name}, address: ${item.address} \n`)
+// // })
+
+// const phone = details.find((detail)=>detail.phone%2===0)
+// console.log (phone.name);
+
